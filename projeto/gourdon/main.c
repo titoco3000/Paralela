686761baab579a2540d0421a1149589a36df3247
titoco3000 <tito.guidotti@gmail.com>
2023-04-26 21:46:56 -0300

    adiciona metodo de gourdon

---
+++ projeto/gourdon/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define PRECISAO 100000
+int main(){
+    int N=PRECISAO,n=N,a[PRECISAO+9],x;
+    while(--n){
+        a[n]=1+1/n;
+    }
+    for(;N>9;printf("%d",x))
+    for(n=N--;--n;a[n]=x%n,x=10*a[n-1]+x/n);
+
+    return 0;
+}
